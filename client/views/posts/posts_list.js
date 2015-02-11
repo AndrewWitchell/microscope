1f19aaaf3fe8c227171ffaf209e7cc86993595ca
Andrew Witchell <andrew@rethrive.co>
2015-02-11 13:23:21 -0500

    Committ 4-4
    Removed `autopublish` and set up a basic publication.

--- client/views/posts/posts_list.js
+++ client/views/posts/posts_list.js
@@ -1,22 +1,8 @@
-var postsData = [
-	{
-		title: 'Introducing Telescope',
-		author: 'Sacha Greif',
-		url: 'http://sachagreif.com/Introducing-telescope/'
-	},
-	{
-		title: 'Meteor',
-		author: 'Tom Coleman',
-		url: 'http://Meteor.com'
-	},
-	{
-		title: 'The Meteor Book',
-		author: 'Tom Coleman',
-		url: 'http://themeteorbook.com'
-	}
-];
+
 
 Template.postsList.helpers({
-	posts: postsData
+	posts: function() {
+		return Posts.find();
+	}
 });
 
